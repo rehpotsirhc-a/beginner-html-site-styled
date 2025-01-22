1a113b6f4e3220771cad8f717123c0de31bb4de3
Slugger <christopher.martinez24a@student.cart.org>
2025-01-22 09:34:14 -0800

    Done

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/god2.jpg") {
+    myImage.setAttribute("src", "images/god2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/god2.jpg");
+  }
+});
+
+//
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+
+  myButton.addEventListener("click", () => {
+    setUserName();
+  });
+
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
